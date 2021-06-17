08f8734c982b165cd8d1662124609c74e0ed3ea9
Pavle09 <ptijanic9@gmail.com>
2021-06-17 17:35:38 +0200

    prevod-settingsUpdate-3

--- resources/views/pages/settings/genres/genres.blade.php
+++ resources/views/pages/settings/genres/genres.blade.php
@@ -108,7 +108,7 @@
                                 </tr>
                                 <!--Modal-->
                                 <div
-                                    class="absolute z-20 top-0 left-0 items-center justify-center hidden w-full h-screen bg-black bg-opacity-10 delete_modal_{{$genre->id}}" id="{{$genre->id}}">
+                                    class="absolute z-20 top-0 left-0 items-center justify-center hidden w-full h-screen bg-black bg-opacity-10 delete-modal_{{$genre->id}}" id="{{$genre->id}}">
                                     <!-- Modal -->
                                     <div class="w-[500px] bg-white rounded shadow-lg md:w-1/3">
                                         <!-- Modal Header -->
