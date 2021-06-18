60d4648aaea787665e7ce33eaed22af9aa2f44b9
Pavle09 <ptijanic9@gmail.com>
2021-06-18 11:35:44 +0200

    modal-update

--- resources/views/pages/students/studentOverdue.blade.php
+++ resources/views/pages/students/studentOverdue.blade.php
@@ -51,8 +51,8 @@
                             <div class="absolute right-0 w-56 mt-[10px] origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none"
                                 aria-labelledby="headlessui-menu-button-1" id="headlessui-menu-items-117" role="menu">
                                 <div class="py-1">
-                                    <a href="{{ route('deleteStudent', ['user' => $user->id]) }}" tabindex="0"
-                                        class="flex w-full px-4 py-2 text-sm leading-5 text-left text-gray-700 outline-none hover:text-blue-600"
+                                    <a href="#" tabindex="0" id="{{$user->id}}"
+                                        class="flex w-full px-4 py-2 text-sm leading-5 text-left text-gray-700 outline-none hover:text-blue-600 show-deleteModal"
                                         role="menuitem">
                                         <i class="fa fa-trash mr-[5px] ml-[5px] py-1"></i>
                                         <span class="px-4 py-0">Izbriši korisnika</span>
@@ -334,6 +334,30 @@
                                         </div>
                                     </td>
                                 </tr>
+                                <!--Modal-->
+                                <div
+                                    class="absolute z-20 top-0 left-0 items-center justify-center hidden w-full h-screen bg-black bg-opacity-10 delete-modal_{{$user->id}}" id="{{$user->id}}">
+                                    <!-- Modal -->
+                                    <div class="w-[500px] bg-white rounded shadow-lg md:w-1/3">
+                                        <!-- Modal Header -->
+                                        <div class="flex items-center justify-between px-[30px] py-[20px] border-b">
+                                            <h3>Da li ste sigurni da želite da izbrišete korisnika?</h3>
+                                            <button class="text-black close cancel focus:outline-none" id="{{$user->id}}">
+                                                <span aria-hidden="true" class="text-[30px]">&times;</span>
+                                            </button>
+                                        </div>
+                                        <!-- Modal Body -->
+                                        <div class="flex items-center justify-center px-[30px] py-[20px] border-t w-100 text-white">
+                                            <a href="{{ route('deleteStudent', ['user' => $user->id]) }}}"
+                                                class=" text-center shadow-lg mr-[15px] w-[150px] focus:outline-none text-sm py-2.5 px-5 transition duration-300 ease-in hover:bg-[#46A149] bg-[#4CAF50] rounded-[5px]">
+                                                <i class="fas fa-check mr-[7px]"></i> Izbriši
+                                            </a>
+                                            <a href="#" id="{{$user->id}}" class="cancel shadow-lg w-[150px] focus:outline-none text-sm py-2.5 px-5 transition duration-300 ease-in rounded-[5px] bg-[#F44336] hover:bg-[#F55549] text-center">
+                                            <i class="fas fa-times mr-[7px]"></i> Poništi 
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
                             @endforeach
                             </tbody>
                         </table>
